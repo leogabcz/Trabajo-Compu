b70d854f5c12d8832ec489a0ea1cbfcae89c8ddf
jandrovm <129178887+jandrovm@users.noreply.github.com>
2023-05-21 13:32:42 +0200

    Update simulation2Dfdtd.py

--- simulation2Dfdtd.py
+++ simulation2Dfdtd.py
@@ -1,12 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 class fdtd2D:
     Lx = 201 #numero de celdas horizontales
     Ly = 201 #numero de celdas verticales
-    ex = np.zeros(Lx-1, Ly)
-    ey = np.zeros(Lx, Ly-1)   #Se declaran todos del mismo tamaño
-    hz = np.zeros(Lx-1, Ly-1)   #luego simplemente se omitiran los ultimos
+    ex = np.zeros((Lx-1, Ly))
+    ey = np.zeros((Lx, Ly-1))   #Se declaran todos del mismo tamaño
+    hz = np.zeros((Lx-1, Ly-1))   #luego simplemente se omitiran los ultimos
     bcl = ''    
     CFL = 0.9
 
@@ -16,13 +15,13 @@ class fdtd2D:
         self.Ly = Ly
         self.CFL = CFL
 
-        self.x = np.linspace(0, 10, Lx)
-        self.y = np.linspace(0, 10, Ly)
+        self.x = np.linspace(0, 1, Lx)
+        self.y = np.linspace(0, 1, Ly)
         self.dx = self.x[1] - self.x[0]
         self.dy = self.y[1] - self.y[0]
         self.xDual = (self.x[1:] + self.x[:-1])/2
         self.yDual = (self.y[1:] + self.y[:-1])/2
-        self.initGauss(self, Lx/20, Lx/20, Lx/2, Ly/2)
+        self.initGauss(Lx/20, Lx/20, Lx/2, Ly/2)
         self.dt = CFL*np.sqrt(self.dx**2 + self.dy**2)
         self.mu = 1
         self.eps = 1
@@ -31,22 +30,41 @@ class fdtd2D:
 
     def sim(self, T):
         
-        exaux=np.zeros(self.x.shape**2)
-        eyaux=np.zeros(self.x.shape**2)
-        hzaux=np.zeros(self.xDual.shape)
-        alpha = 1 - 1/2*self.sigma*self.dt/self.eps
-        beta = 1 + 1/2*self.sigma*self.dt/self.eps
-        #Condicones periodicas por ahora
-        self.ex[:,0]= np.zeros(self.lx-1)
-        self.ey[0,:]= np.zeros(self.ly-1)
-        self.ex[:,-1]= np.zeros(self.lx-1)
-        self.ey[-1,:]= np.zeros(self.ly-1)
-        for t in T:
+        mu = self.mu
+        eps = self.eps
+        dt = self.dt
+        dx = self.dx
+        dy = self.dy
+        c = 1./np.sqrt(eps*mu)
+        sigma = self.sigma
+        alpha = 1 - 1/2*sigma*dt/eps
+        beta = 1 + 1/2*sigma*dt/eps
+        #Condicones PEC por ahora
+        if self.bcl == 'pec':
+            self.ex[:,0]= np.zeros(self.Lx-1)
+            self.ey[0,:]= np.zeros(self.Ly-1)
+            self.ex[:,-1]= np.zeros(self.Lx-1)
+            self.ey[-1,:]= np.zeros(self.Ly-1)
+        for t in range(T):
+            if self.bcl == 'mur':
+                aux1x = self.ex[:,0] #Frontera y = 0
+                aux2x = self.ex[:,-1] #Frontera y = Ly
+                aux3x = self.ex[:,1] #Antes de frontera y = 1 
+                aux4x = self.ex[:,self.Ly-2] #Antes de frontera y = Ly-1
+                aux1y = self.ey[0,:] #Frontera x = 0
+                aux2y = self.ey[-1,:] # Frontera x = Lx
+                aux3y = self.ey[1,:] # Frontera x = 1
+                aux4y = self.ey[self.Lx-2,:] #Frontera x = Lx-1
             #Hay un rotacional, las Ex y dy van alternadas
-            self.hz[:,:] = self.hz[:,:] - self.dt/self.mu/self.dx*( self.ey[1:,:] - self.ey[:-1,:]) - self.dt/self.mu/self.dy*( self.ex[:,1:] - self.ex[:,:-1]) 
-            self.ex[:,1:-1] = alpha/beta*self.ex[:,1:-1] + self.dt/beta/self.eps/self.dy*(self.hz[:,1:] - self.hz[:,:-1])
-            self.ey[1:-1,:] = alpha/beta*self.ey[1:-1,:] + self.dt/beta/self.eps/self.dx*(self.hz[1:,:] - self.hz[:-1,:])
-        
+            self.hz[:,:] = self.hz[:,:] - dt/mu/dx*( self.ey[1:,:] - self.ey[:-1,:]) - dt/mu/dy*( self.ex[:,1:] - self.ex[:,:-1]) 
+            self.ex[:,1:-1] = alpha/beta*self.ex[:,1:-1] + dt/beta/eps/dy*(self.hz[:,1:] - self.hz[:,:-1])
+            self.ey[1:-1,:] = alpha/beta*self.ey[1:-1,:] + dt/beta/eps/dx*(self.hz[1:,:] - self.hz[:-1,:])
+            if self.bcl == 'mur':
+                self.ex[:,0] = aux1x[:] + (c * dt + dy )/(c * dt - dy)*(self.ex[:,0] - aux3x[:])
+                self.ex[:,-1] = aux2x[:] + (c * dt - dy )/(c * dt + dy)*(self.ex[:,self.Ly-2] - aux4x[:])
+                self.ey[0,:] = aux1y[:] + (c * dt + dx )/(c * dt - dx)*(self.ey[1,:] - aux3y[:])
+                self.ey[-1,:] = aux2y[:] + (c * dt - dx )/(c * dt + dx)*(self.ey[self.Lx-2,:] - aux4y[:])
+
     def initGauss(self, sx, sy, x0, y0):
         
         lx = self.Lx
