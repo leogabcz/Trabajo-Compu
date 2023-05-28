bd0f74a89f7a41adc291a4ed36fd2065126f73da
leogabcz <leogabcz@gmail.com>
2023-05-28 21:42:27 +0200

    Cosas para campo en ez

---
+++ Mur2D_ez.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from simulacion2Dez import fdtd2Dez
+
+
+sm = fdtd2Dez(201, 201, 0.95, 'mur2d', 'gausstang')
+
+glocx = np.linspace(0, sm.Lx*sm.dx, sm.Lx-1)    #Genero la dimensión X del espacio de nuestro sistema
+glocy = np.linspace(0, sm.Ly*sm.dy, sm.Ly-1)    #Genero la dimensión Y del espacio de nuestro sistema
+#1 unidad menos de posicion en ambas direcciones porque se representa la célula de hz
+k = 5.
+l = 4.
+ndiv = 5
+
+filtro = np.zeros((len(glocx),len(glocy)), dtype=bool) #Filtro se encarga de quitar las felchas del plot para que no esté
+for i in range(len(filtro)):                           #sobrecargado. Es una matriz de verdadero o falso.
+        for j in range(len(filtro)):
+            if (i % ndiv == 0 and j % ndiv == 0):
+                filtro[i,j] = True
+
+            else:
+                filtro[i,j] = False
+posx = np.zeros((len(glocx),len(glocy)))   #Genera una matriz en la que se muestra la posición de cada celda en el eje X
+posy = np.zeros((len(glocx),len(glocy)))   #Genera una matriz en la que se muestra la posición de cada celda en el eje Y
+vx = np.zeros((len(glocx),len(glocy)))
+vy = np.zeros((len(glocx),len(glocy)))     #Son las componentes X, Y, Z de los vectores, en nuestro caso un vector tendrá 
+vz = np.zeros((len(glocx),len(glocy)))     #sólo X e Y y el otro Z, por lo que no hace falta crear más.
+
+vx[:,:] = 1./2*( sm.hx[:,1:] + sm.hx[:,:-1] )
+vy[:,:] = 1./2*( sm.hy[1:,:] + sm.hy[:-1,:] )
+vz[:,:] = sm.ez[:,:]
+
+#vz = np.zeros((len(glocx),len(glocy)))
+#dirv = np.zeros((len(glocx),len(glocy)))
+
+for i in range(len(glocx)):     #Asigno valores de las posiciones en X
+    posx[i,:] = glocx[i]
+    
+for j in range(len(glocy)):     #Asigno valores de las posiciones en Y
+    posy[:,j] = glocy[j]
+
+            
+figure = plt.figure(figsize=(12,7))  #Aquí genero la figura
+figure.tight_layout()                #Esto deja todo compactado (evita que se pisen mutuamente las gráficas)
+
+ax1 = plt.subplot(1,2,1)
+ax2 = plt.subplot(1,2,2)             #Esto declara los plot que se pintarán por serparado, así se pueden quitar o añadir 
+                                     #fácilmente
+        
+tRange = np.linspace(0, 10, 201)
+energy_list = []
+
+for t in tRange:
+    
+    sm.sim(1)
+    energy_list.append(sm.energy)
+    
+
+    vx[:,:] = 1/2*( sm.hx[:,1:] + sm.hx[:,:-1] )
+    vy[:,:] = 1/2*( sm.hy[1:,:] + sm.hy[:-1,:] )
+    vz[:,:] = sm.ez[:,:]
+    minim = np.nanmin(vz)
+    maxim = np.nanmax(vz)*1.2
+    #Esto es el cálculo de componentes, no forma 
+    #No forma parte de la representación en sí
+    
+    #ax1 es el plot vectorial con el color de fondo, ax2 sólo son las flechas y ax3 los colores con líneal de nivel
+    
+    e_mod = np.sqrt(vx**2 + vy**2)
+    ax1.quiver(posx[filtro],posy[filtro],vx[filtro],vy[filtro],scale=18,angles='xy')
+    im=ax1.imshow(e_mod.T,extent=(0.0,1.0,0.0,1.0),origin='lower',cmap='viridis',vmax=maxim,vmin=-0.3) 
+#    cb1 = plt.colorbar(im, ax=ax1,label=r'|$\vec{E}$|')
+#    ax1.grid()
+    
+    im=ax2.imshow(vz.T,extent=(0.0,1.0,0.0,1.0),cmap='turbo',origin='lower')
+    ax2.contour(posx,posy,vz,4,colors='white',alpha=0.7)
+    #cb2 = plt.colorbar(im, ax=ax2,label=r'H$_z$')
+#    ax2.grid()
+#    plt.grid()
+#    plt.ylim(-0.1, 1.1)
+#    plt.xlim(glocx[0], glocx[-1])
+    plt.pause(0.001) # pausa 0.1s
+    ax1.cla() # cierra los plots anteriores
+    ax2.cla()
+#    ax3.cla()
+#    cb1.remove()
+#    cb2.remove()
+
+fig = plt.figure(figsize=(8,8))
+plt.plot(tRange, energy_list)
+plt.show()
+
+
+print("END")
---
+++ simulacion2Dez.py
@@ -0,0 +1,155 @@
+import numpy as np
+
+class fdtd2Dez:
+
+    def initGauss(self, sx, sy, x0, y0):
+        
+        lx = self.Lx
+        ly = self.Ly
+        i0 = x0/self.dx
+        j0 = y0/self.dy
+        #Modo tangente eléctrico
+        for j in range( ly -1):
+            #El campo x adelantado 1/2 en x (1 posicion en x menos)
+            for i in range( lx -1):
+                self.ez[i,j] = 2*np.exp( -1/2*self.dx*( i - i0 )**2/sx**2  + -1/2*self.dy*( j - j0 )**2/sy**2 )
+        for j in range( ly ):
+            #El campo magnetico en z adelantado diagonalmente
+            for i in range( lx-1 ):
+                self.hx[i,j] = 0
+        for j in range( ly-1 ):
+            #El campo magnetico en z adelantado diagonalmente
+            for i in range( lx ):
+                self.hy[i,j] = 0
+
+
+    def initGaussRadial(self, sx, sy, x0, y0):
+        
+        lx = self.Lx
+        ly = self.Ly
+        i0 = x0/self.dx
+        j0 = y0/self.dy
+        #Modo tangente eléctrico
+        for j in range( ly ):
+            #El campo x adelantado 1/2 en x (1 posicion en x menos)
+            for i in range( lx-1 ):
+                self.ex[i,j] = 2*np.exp( -1/2*self.dx*( i + 1/2 - i0 )**2/sx**2  + -1/2*self.dy*( j - j0 )**2/sy**2 )
+        for j in range( ly-1 ):
+            #El campo y adelantado 1/2 en y (1 posicion menos en y)
+            for i in range( lx ):
+                self.ey[i,j] = 2*np.exp( -1/2*self.dx*( i - i0 )**2/sx**2  + -1/2*self.dy*( j  + 1/2 - j0 )**2/sy**2 )
+        for j in range( ly-1 ):
+            #El campo magnetico en z adelantado diagonalmente
+            for i in range( lx-1 ):
+                self.hz[i,j] = 2.5*np.exp( -1/2*self.dx*( i + 1/2 - i0 )**2/sx**2  + -1/2*self.dy*( j + 1/2 - j0 )**2/sy**2 )
+        
+        
+    def __init__(self, Lx, Ly, CFL, bcl = ['mur', 'pbc', 'pec', 'mur2'], condini = ['gausstang', 'gaussrad']):
+        
+        self.Lx = Lx
+        self.Ly = Ly
+        self.CFL = CFL
+        self.mu = 1
+        self.eps = 1
+        self.sigma = 0
+        self.c = 1/np.sqrt(self.eps*self.mu)
+        self.bcl = bcl
+        self.condini = condini 
+        
+        self.x = np.linspace(0, 1, Lx)
+        self.y = np.linspace(0, 1, Ly)
+        self.dx = self.x[1] - self.x[0]
+        self.dy = self.y[1] - self.y[0]
+        self.dt = CFL/np.sqrt(1/self.dx**2 + 1/self.dy**2)/self.c
+        self.hx = np.zeros((Lx-1,Ly))
+        self.hy = np.zeros((Lx,Ly-1))
+        self.ez = np.zeros((Lx-1,Ly-1))
+        
+        if self.condini == 'gausstang':
+            self.initGauss(0.4, 0.4, 0.5, 0.5)
+        elif self.condini == 'gaussrad':
+            self.initGaussRadial(0.4, 0.4, 0.5, 0.5)
+        
+    def sim(self, T):
+        
+        mu = self.mu
+        eps = self.eps
+        dt = self.dt
+        dx = self.dx
+        dy = self.dy
+        assert(dt != 0)
+        assert(dx != 0)
+        assert(dy != 0)
+        assert(eps != 0)
+        assert(mu != 0)
+        c = 1./np.sqrt(eps*mu)
+        sigma = self.sigma
+        alpha = 1 - 1/2*sigma*dt/eps
+        beta = 1 + 1/2*sigma*dt/eps
+        #Metemos las fronteras
+        frxmin = int(self.Lx/1000)
+        frxmax = self.Lx - 2
+        frymin = int(self.Ly/1000)
+        frymax = self.Ly - 2
+        assert(frxmin >= 0)
+        assert(frxmax < self.Lx and frxmax > frxmin)
+        assert(frymin >= 0)
+        assert(frymax < self.Ly and frymax > frymin)
+
+        if self.bcl == 'pec':
+            self.ez[:,0]= np.zeros(self.Ly - 1)
+            self.ez[-1,:]= np.zeros(self.Lx - 1)
+            self.ez[:,-1]= np.zeros(self.Ly - 1)
+            self.ez[0,:]= np.zeros(self.Lx - 1)
+        for t in range(T):
+            if self.bcl == 'mur' or self.bcl == 'mur2':
+                aux1 = self.ez[:,frymin] #Frontera OY inferior
+                aux2 = self.ez[:,frymax] #Frontera OY superior
+                aux3 = self.ez[:,frymin + 1] #Antes de frontera OY inf 
+                aux4 = self.ez[:,frymax - 1] #Antes de frontera OY sup
+                aux5 = self.ez[frxmin,:] #Frontera OX izda
+                aux6 = self.ez[frxmax,:] # Frontera OX dhca
+                aux7 = self.ez[frxmin + 1,:] #Antes de frontera OX izda 
+                aux8 = self.ez[frxmax - 1,:] #Antes de frontera OX dcha
+            elif self.bcl == 'pec':
+                 self.ez[:,0]= np.zeros(self.Lx - 1)
+                 self.ez[-1,:]= np.zeros(self.Ly - 1)
+                 self.ez[:,-1]= np.zeros(self.Lx - 1)
+                 self.ez[0,:]= np.zeros(self.Ly - 1)
+            
+            #Hay un rotacional, las Ex y dy van alternadas
+            self.hx[:,1:-1] =  self.hx[:,1:-1] - dt/mu/dx*( self.ez[:,1:] - self.ez[:,:-1]) 
+            self.hy[1:-1,:] =  self.hy[1:-1,:] + dt/mu/dx*( self.ez[1:,:] - self.ez[:-1,:]) 
+            self.ez[:,:] = alpha/beta*self.ez[:,:] + dt/beta/eps/dy*(self.hy[1:,:] - self.hy[:-1,:]) - dt/beta/eps/dy*(self.hx[:,1:] - self.hx[:,:-1])
+            
+            
+            if self.bcl == 'mur':
+                self.ez[:,frymin] = aux3[:] + (c * dt - dy )/(c * dt + dy)*(self.ez[:,frymin+1] - aux1[:])
+                self.ez[:,frymax] = aux4[:] + (c * dt - dy )/(c * dt + dy)*(self.ez[:,frymax-1] - aux2[:])
+                self.ez[frxmin,:] = aux7[:] + (c * dt - dy )/(c * dt + dy)*(self.ez[frxmin,:] - aux5[:])
+                self.ez[frxmax,:] = aux8[:] + (c * dt - dy )/(c * dt + dy)*(self.ez[frxmax,:] - aux6[:])
+
+            if self.bcl == 'mur2':
+                self.ez[:,frymin] = aux3[:] + (c * dt - dy )/(c * dt + dy)*(self.ez[:,frymin+1] - aux1[:]) + mu*c/ 2*(dy + c*dt) * (self.hy[1:,frymin] - self.hy[:-1,frymin] + self.hy[1:,frymin+1] - self.hy[:-1,frymin+1])
+                self.ez[:,frymax] = aux4[:] + (c * dt - dy )/(c * dt + dy)*(self.ez[:,frymax-1] - aux2[:]) + mu*c/ 2*(dy + c*dt) * (self.hy[1:,frymax] - self.hy[:-1,frymax] + self.hy[1:,frymax-1] - self.hy[:-1,frymax-1])
+                self.ez[frxmin,:] = aux7[:] + (c * dt - dy )/(c * dt + dy)*(self.ez[frxmin,:] - aux5[:]) - mu*c/ 2*(dy + c*dt) * (self.hx[frxmin,1:] - self.hx[frxmin,:-1] + self.hx[frxmin+1,1:] - self.hx[frxmin+1,:-1])
+                self.ez[frxmax,:] = aux8[:] + (c * dt - dy )/(c * dt + dy)*(self.ez[frxmax,:] - aux6[:]) - mu*c/ 2*(dy + c*dt) * (self.hx[frxmax,1:] - self.hx[frxmax,:-1] + self.hx[frxmax-1,1:] - self.hx[frxmax-1,:-1])
+
+     
+        self.energy = self.energia(frxmin, frxmax, frymin, frymax)        
+    
+    
+    def energia(self, frxmin, frxmax, frymin, frymax):
+        
+        eps = self.eps
+        mu = self.mu
+        
+        h2x = self.hx*self.hx
+        h2y = self.hy*self.hy
+        e2z = self.ez*self.ez
+
+        energy = 1 * eps *(np.sum(e2z[frxmin:frxmax]))
+        return energy               
+        
+        
+    
